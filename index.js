9751e04c16781ee3e31fd5c6b3eb31a1b552b9ef
Wajih Hassouneh <wajihmohd>
2022-02-20 08:25:58 -0500

    Adding home page view

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world Wajih!</h1>')
+    res.send('<h1>Home</h1>')//when put render gives an error
 })
 
 app.get('*', (req, res) => {
