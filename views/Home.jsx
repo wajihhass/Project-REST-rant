91e1d4e0644dda529f3100b301907085562ef26b
Wajih <hass_wajih@hotmail.com>
2022-02-22 15:57:54 -0500

    Adding and rendering a new view

--- views/Home.jsx
+++ views/Home.jsx
@@ -22,38 +22,3 @@ function home () {
 
 module.exports = home
 
-/*
-/*88888  <main>
-    <h1>HOME</h1>
-    <div>
-      <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-      <div>
-        Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
-      </div>
-    </div>
-    <a href="/places">
-        <button className="btn-primary">Places Page</button>
-    </a>
-</main>*/
-/*
-const React = require('react')
-const Def = require('./default')
-
-function home () {
-    return (
-        <Def>
-            <main>
-                <h1>HOME</h1>
-                <div>
-                  <img src="/images/chia-fruit-drink.jpg" alt="Chia Fruit Shake" />
-                </div>
-                <a href="/places">
-                    <button className="btn-primary">Places Page</button>
-                </a>
-            </main>
-        </Def>
-    )
-}
-
-module.exports = home
-*/
