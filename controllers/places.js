ec30ccd816dddfae1da673e9f2941c1bc1097625
Wajih <hass_wajih@hotmail.com>
2022-02-22 15:54:18 -0500

    Adding and rendering a new view

--- controllers/places.js
+++ controllers/places.js
@@ -4,4 +4,9 @@ router.get('/', (req, res) => {
     res.render('Places/index', { places: [] })
 })
 
+
+/* wajih*/
+router.get('/new', (req, res) => {
+    res.render('places/new')
+  })
 module.exports = router
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // Modules and Globals
 require('dotenv').config()
 const express = require('express')
+const res = require('express/lib/response')
 const app = express()
 
 // Express Settings
@@ -8,7 +9,6 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
-app.use(express.static('views'))
 
 // Controllers & Routes
 app.use('/places', require('./controllers/places'))
@@ -21,5 +21,8 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+app.get('/new', (req,res)=>{
+    res.render('places/new')
+})
 // Listen for Connections
 app.listen(process.env.PORT)
